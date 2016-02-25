ea43ad6c230abbd92490d2cb2fb75d014b49bb44
tabbyz <tabbyz@users.noreply.github.com>
2016-02-25 14:38:48 +0900

    Add 'mailer.yml.example'

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,5 @@
 unless Chibineko::Application.config.action_mailer.nil?
-  options = YAML.load_file(Rails.root.join('config', 'mailer.yml'))[Rails.env]
-  Chibineko::Application.config.action_mailer.merge! options.deep_symbolize_keys! unless options.nil?
+  if options = YAML.load_file(Rails.root.join("config", "mailer.yml"))[Rails.env]
+    Chibineko::Application.config.action_mailer.merge! options.deep_symbolize_keys!
+  end
 end
