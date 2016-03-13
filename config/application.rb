c7c4c401de15ccc88fcd33da2c88a79186f0d5c4
tabbyz <tabbyz@users.noreply.github.com>
2016-03-13 20:17:05 +0900

    Update config files

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,5 @@ module Chibineko
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.initialize_on_precompile = true
-    
-    config.serve_static_assets = true
   end
 end
